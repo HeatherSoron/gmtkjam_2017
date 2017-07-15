01bcfdcb6d37135d74351332d8d36ec0fc3124c3
Ethan "Soron" Kaminski <ethan.kaminski@gmail.com>
2017-07-15 14:02:10 +1000

    Player looks at mouse position

--- js/player.js
+++ js/player.js
@@ -2,13 +2,21 @@ Class.makeClass(null, function Player(x, y) {
 	this.body = new GfxRect(x, y, 10, 10);
 })
 
-var offset = 0;
 Player.prototype.render = function() {
 	this.body.render();	
+
+	var dx = mouseState.x - this.body.x;
+	var dy = mouseState.y - this.body.y;
+
+	var angle = Math.atan2(dy, dx); // oh thank goodness for atan2
+	if (dx == 0 && dy == 0) {
+		angle = 0;
+	}
+
 	ctx.save()
 	ctx.translate(this.body.x, this.body.y);
 	ctx.fillStyle = 'green';
-	ctx.rotate((offset++)/8);
-	ctx.fillRect(-2, -10, 4, 4);
+	ctx.rotate(angle);
+	ctx.fillRect(-2 + 8, -2, 4, 4);
 	ctx.restore();
 }
