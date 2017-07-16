4a7433d4e056825a7bc088b08be66b1f3131b92a
Ethan "Soron" Kaminski <ethan.kaminski@gmail.com>
2017-07-16 23:21:43 +1000

    Remove obsolete player mouse-look indicator

--- js/player.js
+++ js/player.js
@@ -45,21 +45,6 @@ Player.prototype.render = function() {
 	this.body.render();
 	ctx.restore();
 
-	var dx = mouseState.x - this.body.x;
-	var dy = mouseState.y - this.body.y;
-
-	var angle = Math.atan2(dy, dx); // oh thank goodness for atan2
-	if (dx == 0 && dy == 0) {
-		angle = 0;
-	}
-
-	ctx.save()
-	ctx.translate(this.body.x, this.body.y);
-	ctx.fillStyle = 'green';
-	ctx.rotate(angle);
-	ctx.fillRect(-2 + 8, -2, 4, 4);
-	ctx.restore();
-
 	if (this.anchor) {
 		this.drawGrapple();
 	}
